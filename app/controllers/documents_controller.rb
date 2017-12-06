dcd99ff0f122ae0480b7b7f5f231db764c3cf4cd
hortense gilbert <hortensegilbert@hotmail.com>
2017-12-06 18:20:39 +0100

    can delete doc, fixed croix to add

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -59,6 +59,12 @@ class DocumentsController < ApplicationController
     redirect_to document_path(@document)
   end
 
+  def destroy
+    @document = Document.find(params[:id])
+    @document.destroy
+    redirect_to documents_path
+  end
+
 
   private
 
