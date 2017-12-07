6add50781102283c596c82eea9c1a86f249dd03e
hortense gilbert <hortensegilbert@hotmail.com>
2017-12-07 19:17:29 +0100

    ok

--- app/javascript/components/dropzone.js
+++ app/javascript/components/dropzone.js
@@ -5,12 +5,11 @@ function newdropzone() {
   Dropzone.autoDiscover = false;
   // const myDropzone = new Dropzone("#doc-dropzone");
   const myDropzone = new Dropzone("#doc-dropzone", {
-    dictDefaultMessage: "Here... " }
-    );
+    dictDefaultMessage: "Here... "
+    });
 
   myDropzone.on("success", function(file, response) {
     addinput_in_form(response);
-    console.log("ok");
     document.getElementById("btnsavedocs").disabled = false;
     });
 
@@ -20,7 +19,6 @@ function addinput_in_form(response) {
   const doc_id = response.id;
   const form = document.querySelector(".batch-update-form");
   const html = `<input name="document_ids[]" type="hidden" value=${doc_id} />`;
-  console.log(html);
   form.insertAdjacentHTML("afterbegin", html);
 }
 
--- app/javascript/components/tag.js
+++ app/javascript/components/tag.js
@@ -1,16 +1,5 @@
 const urlstring = window.location.href;
 
-// get selected tags from query in params
-// function getParameterByName(name, url) {
-//     if (!url) url = window.location.href;
-//     name = name.replace(/[\[\]]/g, "\\$&");
-//     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-//         results = regex.exec(url);
-//     if (!results) return null;
-//     if (!results[2]) return '';
-//     return decodeURIComponent(results[2].replace(/\+/g, " "));
-// }
-
 function generateParameters() {
   const selectedtags = [];
   document.querySelectorAll(".tag-s").forEach(tag => {
@@ -18,15 +7,6 @@ function generateParameters() {
   });
   return selectedtags;
 }
-
-// adding class "tag-s" to all selected tags
-// function showSelectedTag() {
-//   selectedtags.split(" ").forEach(function(tag) {
-//     if (document.getElementById(tag) !== null) {
-//         document.getElementById(tag).classList.add("tag-s");
-//     }
-//   });
-// }
 
 // Ajout de tagName si non inclut ou suppression tagname si inclut
 function updateTagsArray(tagArray, tagName) {
@@ -55,9 +35,5 @@ function listentag() {
     document.querySelector(".searchtag form").submit();
   }));
 }
-
-// if (selectedtags !== null) {
-//     showSelectedTag();
-// }
 
 listentag();
