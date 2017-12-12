2d7adf6b30c0e6e2cbb525719dcb651c3093f61c
arthur reboul <arthur.reboul@gmail.com>
2017-12-12 19:00:31 +0100

    document scrapper operational

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,15 +4,16 @@ class TagsController < ApplicationController
     @document = Document.find(params[:document_id])
 
     authorize @tag
+    # get user query and replace " " by _
     @tag_name = params[:tag][:name].downcase.gsub(/\s/, "_")
-
+    # check if tag already exist by searching by tag.name instead of tag.id, if it doesn't exist create it
     if Tag.tag_from_tagnames([@tag_name]).first.nil?
       @tag.name = @tag_name
       @tag.save
     else
       @tag = Tag.tag_from_tagnames([@tag_name]).first
     end
-
+    # check if document doesn't already include the tag, if it doesn't create a new doctag
     if !@document.tags.include?(@tag)
       @doctag = Doctag.new
       @doctag.document = @document
