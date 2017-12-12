1eb7a9a32aca5373212f38172d5f4d6cdf78d91c
hortense gilbert <hortensegilbert@hotmail.com>
2017-12-12 16:00:02 +0100

    before pull

--- app/javascript/components/select2.js
+++ app/javascript/components/select2.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import 'select2';
 
+// use in index documents page to search for a tag/////
 $('.select2').select2({
     multiple: true,
     width: '200px',
@@ -8,7 +9,25 @@ $('.select2').select2({
 });
 
 $('.select2').on('select2:select', function (e) {
-  document.getElementById('submittag').submit()
+  document.getElementById('submittag').submit();
 });
+
+
+// use in new document//////////////////////////////////
+
+$('.select2_new').select2({
+    multiple: true,
+    width: '100%',
+    placeholder: "",
+});
+
+// add tag-s on
+$('.select2_new').on('select2:select', function (e) {
+  document.getElementById(e.params.data.text).classList.toggle("tag-s");
+});
+
+
+
+ ////////////////////////////////////////////
 
 import 'select2/dist/css/select2.css';
